ea9269a5d239936de23da89d599303ab42a1f817
Glenn Moss <glenn.moss@kount.com>
2018-08-06 17:44:16 +0000

    Improved tpm install

--- _tmux/_install.sh
+++ _tmux/_install.sh
@@ -1,14 +1,14 @@
-[[ -d $HOME/.tmux/plugins/tpm ]] || {
-  tmux_ver=$(tmux -V)
-  if [[ $tmux_ver == "tmux 2."* ]]; then
+tmux_ver=$(tmux -V)
+if [[ $tmux_ver == "tmux 2."* ]]; then
+  [[ -d $HOME/.tmux/plugins/tpm ]] || {
     mkdir -p $HOME/.tmux/plugins
-    git clone git@github.com:tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+    git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+  }
 
-    tmux new-session -d -s install-plugins
-    $HOME/.tmux/plugins/tpm/scripts/install_plugins.sh
-    tmux kill-session -t install-plugins
-  else
-    log_warning "Requires tmux 2 (found $tmux_ver)"
-  fi
-  unset tmux_ver
-}
+  tmux new-session -d -s install-plugins
+  $HOME/.tmux/plugins/tpm/scripts/install_plugins.sh
+  tmux kill-session -t install-plugins
+else
+  log_warning "Requires tmux 2 (found $tmux_ver)"
+fi
+unset tmux_ver

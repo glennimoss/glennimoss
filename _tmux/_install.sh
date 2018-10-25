29a895e5891b051d2a38339adc5cf30a8e14d3e2
Glenn Moss <gim@keynetics.com>
2018-10-25 13:04:18 -0700

    Change tmux repo url so it doesn't require ssh key.

--- _tmux/_install.sh
+++ _tmux/_install.sh
@@ -2,7 +2,7 @@
   tmux_ver=$(tmux -V)
   if [[ $tmux_ver == "tmux 2."* ]]; then
     mkdir -p $HOME/.tmux/plugins
-    git clone git@github.com:tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+    git clone https://github.com/tmux-plugins/tpm.git $HOME/.tmux/plugins/tpm
 
     tmux new-session -d -s install-plugins
     $HOME/.tmux/plugins/tpm/scripts/install_plugins.sh
